20aafbbb0b0b1877ca353b21546b7cbd6873d724
Ryan Gallagher <64330673+synapse187@users.noreply.github.com>
2021-02-27 21:40:17 -0500

    Add project files.

---
+++ CsUWPApp01/CharacterSheet.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CsUWPApp01
+{
+    public class CharacterSheet : INotifyPropertyChanged
+    {
+        public CharacterSheet()
+        {
+            Strength = 10;
+        }
+        public int Strength
+        {
+            get => Strength;
+            set
+            {
+                if(Strength != value)
+                {
+                    Strength = value;
+                    this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Strength"));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
